322c890e0f83f903c16034e7cb63f6bf107a1e3d
d-german <facmet107@gmail.com>
2018-07-06 22:12:37 -0500

    made some changes

---
+++ WpfPieViewer/Services/IPieRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WpfPieViewer.Models;
+
+namespace WpfPieViewer.Services
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
---
+++ WpfPieViewer/Services/PieApiRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WpfPieViewer.Models;
+
+namespace WpfPieViewer.Services
+{
+    public class PieApiRepository : IPieRepository
+    {
+        static HttpClient client = new HttpClient();
+        static async Task<IEnumerable<Pie>> GetProductAsync(string path)
+        {
+            IEnumerable <Pie> pies = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                pies = await response.Content.ReadAsAsync<IEnumerable<Pie>>();
+            }
+            return pies;
+        }
+
+        public PieApiRepository()
+        {
+            client.BaseAddress = new Uri("http://localhost:64195/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
